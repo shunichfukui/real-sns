6f4079d971d8cbb673153efd34f11394fc69f6ff
shunich <fukusyun3@gmail.com>
2022-07-06 08:49:30 +0900

    connectDB

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express();
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const authRoute = require("./routes/auth");
-
 const PORT = 3000;
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// データベース接続
+mongoose.connect(process.env.MONGOURL).then(() => {
+    console.log("DBと接続中...")
+}).catch((err) => {
+    console.log(err)
+});
 
 // ミドルウェア
 app.use("/api/users", userRoute);
