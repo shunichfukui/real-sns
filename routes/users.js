c6040a061986f6760f2765c66f8ab2c5d4200908
shunich <fukusyun3@gmail.com>
2022-07-06 10:32:25 +0900

    feat: ユーザー情報更新、削除、取得のapi作成

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,49 @@
 const router = require("express").Router();
+const User = require("../modules/User")
 
-router.get("/", (req, res) => {
-    res.send("user router")
-});
+// CRUD
+// ユーザー情報の更新
+router.put("/:id", async(req, res) => {
+    if(req.body.userId == req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("ユーザー情報が更新されました");
+
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("あなたは自分のアカウントの時だけ情報を更新できます");
+    }
+})
+
+// ユーザー情報の削除
+router.delete("/:id", async(req, res) => {
+    if(req.body.userId == req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("ユーザー情報が削除されました");
+
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("あなたは自分のアカウントの時だけ情報を削除できます");
+    }
+})
+
+// ユーザー情報の取得
+router.get("/:id", async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...othre } = user._doc;
+        res.status(200).json(othre);
+
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 
 module.exports = router;
