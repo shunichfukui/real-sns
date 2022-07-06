dd559c09b423217291667cb039271b3020e67990
shunich <fukusyun3@gmail.com>
2022-07-06 13:24:10 +0900

    feat: 投稿作成、編集、削除、取得api作成

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,57 @@
 const router = require("express").Router();
+const Post = require("../modules/Post");
 
-router.get("/", (req, res) => {
-    res.send("post router")
+// 投稿を作成する
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        return res.status(200).json(savedPost);
+    } catch (err) {
+        return res.status(403).json(err);
+    }
+})
+
+// 投稿を編集する
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.updateOne({
+                $set: req.body,
+            });
+            return res.status(200).json("投稿の編集に成功しました！")
+        } else {
+            return res.status(403).json("他ユーザーの投稿は編集できません。")
+        }
+    } catch (err) {
+        return res.status(403).json(err);
+    }
+})
+
+// 投稿を削除する
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.deleteOne();
+            return res.status(200).json("投稿の削除に成功しました！")
+        } else {
+            return res.status(403).json("他ユーザーの投稿は削除できません。")
+        }
+    } catch (err) {
+        return res.status(403).json(err);
+    }
+})
+
+// 投稿を取得する
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        return res.status(200).json(post)
+    } catch (err) {
+        return res.status(403).json(err);
+    }
 });
 
 module.exports = router;
